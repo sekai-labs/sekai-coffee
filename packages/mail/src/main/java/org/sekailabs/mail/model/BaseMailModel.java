a6cbe187d4dafdbf6a93a7e35c6fa28ecb474f39
AnataAria <kakata28419@gmail.com>
2024-08-31 18:57:22 +0700

    Init

---
+++ packages/mail/src/main/java/org/sekailabs/mail/model/BaseMailModel.java
@@ -0,0 +1,14 @@
+package org.sekailabs.mail.model;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Builder
+@NoArgsConstructor
+@Getter
+@Setter
+public class BaseMailModel {
+    
+}
